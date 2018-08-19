cb85eb80bdc4f29583bb3cd63e8708171559dfbb
xandeer <kkxandeer@gmail.com>
2018-08-19 16:59:32 +0800

    delete deploy.sh

--- ghh/ghh.go
+++ ghh/ghh.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
+	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 
 	"gopkg.in/go-playground/webhooks.v5/github"
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
 )
 
 func main() {
@@ -27,17 +32,54 @@ func main() {
 			name := push.Repository.Name
 			url := push.Repository.CloneURL
 
-			fmt.Printf("branch: %+v\n", branch)
+			_, err := git.PlainClone(name, false, &git.CloneOptions{
+				URL:               url,
+				RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
+			})
 
-			cmdStr := fmt.Sprintf("./deploy.sh ./ %s %s", name, url)
-			cmd := exec.Command("/bin/sh", "-c", cmdStr)
-			_, err := cmd.Output()
-
-			if err != nil {
-				println(err.Error())
+			if err == nil {
+				make(name)
 				return
+			}
+
+			if err == git.ErrRepositoryAlreadyExists {
+				r, e := git.PlainOpen(name)
+				if e == nil {
+					w, err := r.Worktree()
+
+					if err == nil {
+						err = w.Checkout(&git.CheckoutOptions{
+							Hash: plumbing.NewHash(branch),
+						})
+
+						if err == nil {
+							make(name)
+						}
+					}
+				}
 			}
 		}
 	})
 	http.ListenAndServe(":3001", nil)
+}
+
+func fixRelativeDir(dir string) string {
+	if !(strings.HasPrefix(dir, "~") || strings.HasPrefix(dir, "/")) {
+		d, _ := os.Getwd()
+		return filepath.Join(d, dir)
+	}
+	return dir
+}
+
+func make(dir string) {
+	cmd := exec.Command("/bin/sh", "-c", "cd "+dir+" && pwd && make")
+	fmt.Printf("cmd dir: %s\n", cmd.Dir)
+	fmt.Printf("cmd: %v\n", cmd)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		println(err)
+	}
+	fmt.Printf("Output: %s\n", out.String())
 }
