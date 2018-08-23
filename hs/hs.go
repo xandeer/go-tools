bf6d2d8deb861b6aeb7aa6c2ef71600d705d7247
Kevin <kkxandeer@gmail.com>
2018-08-23 14:47:52 +0800

    add upload function

--- hs/hs.go
+++ hs/hs.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -14,6 +15,7 @@ import (
 )
 
 var (
+	ip   string
 	port int
 	dir  string
 )
@@ -40,14 +42,15 @@ func main() {
 	boldcyan.Printf("http://0.0.0.0:%d\n", port)
 
 	fileHandler := http.FileServer(http.Dir(dir))
-	wrappedHandler := loggingHandler(fileHandler)
-	http.Handle("/", wrappedHandler)
+	http.Handle("/", withLogging(fileHandler))
+	http.Handle("/u", withLogging(http.HandlerFunc(upload)))
+
 	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
 		panic(err)
 	}
 }
 
-func loggingHandler(h http.Handler) http.Handler {
+func withLogging(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 		h.ServeHTTP(w, r)
@@ -64,6 +67,7 @@ func loggingHandler(h http.Handler) http.Handler {
 func bindFlags() {
 	flag.IntVar(&port, "p", 9999, "Port")
 	flag.StringVar(&dir, "d", "./", "Directory")
+	flag.StringVar(&ip, "i", "127.0.0.1", "IP or domain name")
 	flag.Parse()
 }
 
@@ -86,4 +90,50 @@ func validateDir(dir string) error {
 		return errors.New(e)
 	}
 	return nil
+}
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	tpl := `<html>
+			<head>
+				<title>Upload a File</title>
+			</head>
+			<body>
+				<form enctype="multipart/form-data" action="http://%v:%v/u" method="post">
+					<input type="file" name="uploadfile" required="true" />
+					<input type="submit" value="upload file" />
+				</form>
+			</body>
+		</html>`
+	html := []byte(fmt.Sprintf(tpl, ip, port))
+	if r.Method == "GET" {
+		w.Write(html)
+	} else if r.Method == "POST" {
+		start := time.Now()
+
+		file, handler, err := r.FormFile("uploadfile")
+		if err != nil {
+			boldred.Println(err)
+			return
+		}
+		defer file.Close()
+
+		path := dir + "/upload/"
+		os.MkdirAll(path, os.ModePerm)
+		f, err := os.OpenFile(path+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			boldred.Println(err)
+			return
+		}
+		defer f.Close()
+		io.Copy(f, file)
+		boldwhite.Printf(
+			"%s\t%s\t%s\tupload file: %s\t%s\n",
+			start.Format("06-01-02 15:04:05"),
+			r.RemoteAddr,
+			r.RequestURI,
+			handler.Filename,
+			time.Since(start),
+		)
+		w.Write(html)
+	}
 }
